886183dcc1b79486b3b04916a109364edf49e09f
Víctor Peralta <victorperalta.correo@gmail.com>
2019-10-14 03:58:44 +0200

    Mostrar PATH para comprobar que texlive esta incluido desde travis.yml. avanza en #12

--- texlive_install.sh
+++ texlive_install.sh
@@ -4,10 +4,3 @@ wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar -xzf install-tl-unx.tar.gz
 cd install-tl*
 ./install-tl --profile=../texlive.profile
-
-PATH=/usr/local/texlive/2019/bin/x86_64-linux:$PATH; export PATH
-MANPATH=/usr/local/texlive/2019/texmf-dist/doc/man:$MANPATH; export MANPATH
-INFOPATH=/usr/local/texlive/2019/texmf-dist/doc/info:$INFOPATH; export INFOPATH
-
-which pdflatex
-echo $PATH

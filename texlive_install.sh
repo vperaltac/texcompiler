fcb388082b10b476980a03574837222fe2ded8ee
Víctor Peralta <victorperalta.correo@gmail.com>
2019-10-14 03:49:47 +0200

    Mostrar PATH para comprobar que texlive esta incluido. avanza en #12

--- texlive_install.sh
+++ texlive_install.sh
@@ -9,12 +9,5 @@ PATH=/usr/local/texlive/2019/bin/x86_64-linux:$PATH; export PATH
 MANPATH=/usr/local/texlive/2019/texmf-dist/doc/man:$MANPATH; export MANPATH
 INFOPATH=/usr/local/texlive/2019/texmf-dist/doc/info:$INFOPATH; export INFOPATH
 
-tlmgr install   \
-  exam          \
-  amsfonts      \
-  stmaryrd      \
-  amsmath
-
-tlmgr update --self --all --no-auto-install
-
 which pdflatex
+echo $PATH

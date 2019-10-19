6c50e224c382652184eeaab514fb546ba9d2f2c9
Víctor Peralta <victorperalta.correo@gmail.com>
2019-10-19 04:11:15 +0200

    modificado path al perfil de texlive. Avanza en #12

--- scripts/texlive_install.sh
+++ scripts/texlive_install.sh
@@ -3,4 +3,4 @@
 wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar -xzf install-tl-unx.tar.gz
 cd install-tl*
-./install-tl --profile=../texlive.profile
+./install-tl --profile=../scripts/texlive.profile

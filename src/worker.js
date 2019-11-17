e2dd8b8a1d1244fb89c698e5a9a99f7ee00d1189
Víctor Peralta <victorperalta.correo@gmail.com>
2019-11-17 02:55:23 +0100

    worker funcionando correctamente. closes #27

--- src/worker.js
+++ src/worker.js
@@ -2,35 +2,39 @@
 const amqp = require('amqplib/callback_api');
 const texCompiler = require('./texCompiler'); 
 
-amqp.connect('amqp://localhost', function(error0, connection) {
-    if (error0)
-        throw error0;
+function worker(){
+    amqp.connect('amqp://localhost', function(error0, connection) {
+        if (error0)
+            throw error0;
 
-    connection.createChannel(function(error1, channel) {
-        if (error1) 
-            throw error1;
+        connection.createChannel(function(error1, channel) {
+            if (error1) 
+                throw error1;
+                
+            var queue = 'compiler_queue';
+            channel.assertQueue(queue, {
+                durable: false
+            });
+            channel.prefetch(1);
+            channel.consume(queue, function reply(msg){
+                let fuente = msg.content.toString();
+                texCompiler(fuente,false)
+                .then(r =>{
+                    let pdf = fuente.replace(".tex", ".pdf");
+
+                    channel.sendToQueue(msg.properties.replyTo,
+                        Buffer.from(pdf.toString()), {
+                            correlationId: msg.properties.correlationId
+                        });
             
-        var queue = 'rpc_queue';
-        channel.assertQueue(queue, {
-            durable: false
-        });
-        channel.prefetch(1);
-        channel.consume(queue, function reply(msg){
-            let fuente = msg.content.toString();
-            texCompiler(fuente,false)
-            .then(r =>{
-                let pdf = fuente.replace(".tex", ".pdf");
-
-                channel.sendToQueue(msg.properties.replyTo,
-                    Buffer.from(pdf.toString()), {
-                        correlationId: msg.properties.correlationId
-                    });
-        
-                channel.ack(msg);
-            })
-            .catch(e =>{
-                throw e;
+                    channel.ack(msg);
+                })
+                .catch(e =>{
+                    throw e;
+                });
             });
         });
     });
-});
+}
+
+module.exports = worker

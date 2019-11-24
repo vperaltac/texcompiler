f4db7875113917d40c95ac9a2509c62702b47773
Víctor Peralta <victorperalta.correo@gmail.com>
2019-11-24 17:47:12 +0100

    nuevas rutas documentadas. Avanza en #41

--- src/index.js
+++ src/index.js
@@ -36,6 +36,16 @@ app.get('/status',(req,res) => {
     res.status(200).json({status: 'OK'})
 });
 
+/**
+ * @api {get} /tex/:nombre/:usuario 
+ * @apiName getTexFile
+ * @apiGroup Subida y bajada de archivos
+ * 
+ * @apiParam {String} nombre Nombre del archivo
+ * @apiParam {String} usuario Nombre de usuario
+ *
+ * @apiSuccess {File} archivo TEX
+ */
 app.get('/tex/:nombre/:usuario',(req,res) => {
     let path = files.getTexPath(req.params.nombre,req.params.usuario);
 
@@ -45,42 +55,95 @@ app.get('/tex/:nombre/:usuario',(req,res) => {
         res.download(path);
 });
 
+/**
+ * @api {get} /listar-tex/:usuario
+ * @apiName listarTex
+ * @apiGroup listado de archivos en el servidor
+ * 
+ * @apiParam {String} usuario Nombre de usuario
+ *
+ * @apiSuccess {json} archivos tex del usuario dado en el servidor.
+ */
 app.get('/listar-tex/:usuario',(req,res) => {
     let listado = files.listarArchivos(req.params.usuario,true,false);
 
     res.send(listado);
 });
 
+/**
+ * @api {get} /listar-pdf/:usuario
+ * @apiName listarPDF
+ * @apiGroup listado de archivos en el servidor
+ * 
+ * @apiParam {String} usuario Nombre de usuario
+ *
+ * @apiSuccess {json} archivos PDF del usuario dado en el servidor.
+ */
 app.get('/listar-pdf/:usuario', (req,res) => {
     let listado = files.listarArchivos(req.params.usuario,false,true);
 
     res.send(listado);
 });
 
+/**
+ * @api {get} /listar/:usuario
+ * @apiName listar
+ * @apiGroup listado de archivos en el servidor
+ * 
+ * @apiParam {String} usuario Nombre de usuario
+ *
+ * @apiSuccess {json} todos los archivos del usuario dado en el servidor.
+ */
 app.get('/listar/:usuario', (req,res) => {
     let listado = files.listarArchivos(req.params.usuario,true,true);
     
     res.send(listado);
 });
 
+/**
+ * @api {get} /listar/:usuario
+ * @apiName listarTodos
+ * @apiGroup listado de archivos en el servidor
+ *
+ * @apiSuccess {json} todos los archivos de todos los usuarios en el servidor.
+ */
 app.get('/listar', (req,res) => {
     let todos = files.listarTodos();
 
     res.send(todos);
 });
 
+/**
+ * @api {delete} /tex/:nombre/:usuario
+ * @apiName eliminarTex
+ * @apiGroup eliminar Archivos
+ * 
+ * @apiParam {String} nombre Nombre del archivo
+ * @apiParam {String} usuario Nombre de usuario
+ *
+ * @apiSuccess {String} mensaje confirmando que el archivo ha sido eliminado.
+ */
 app.delete('/tex/:nombre/:usuario', (req,res) => {
     files.eliminarTex(req.params.nombre,req.params.usuario);
 
     res.send("Archivo eliminado.");
 });
 
+/**
+ * @api {delete} /tex/:nombre/:usuario
+ * @apiName eliminarPDF
+ * @apiGroup eliminar Archivos
+ * 
+ * @apiParam {String} nombre Nombre del archivo
+ * @apiParam {String} usuario Nombre de usuario
+ *
+ * @apiSuccess {String} mensaje confirmando que el archivo ha sido eliminado.
+ */
 app.delete('/pdf/:nombre/:usuario', (req,res) => {
     files.eliminarPDF(req.params.nombre,req.params.usuario);
 
     res.send("Archivo eliminado.");
 });
-
 
 /**
  * @api {post} /compilar Compila un archivo en formato TEX a un documento PDF

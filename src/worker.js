af5ccbb7f515f2fc8717d0951990edf7d3d7eb3c
Víctor Peralta <victorperalta.correo@gmail.com>
2019-11-25 02:29:12 +0100

    worker ya no es una funcion

--- src/worker.js
+++ src/worker.js
@@ -13,41 +13,37 @@ const queue = 'compiler_queue';
 
 // Recibe un mensaje a través de la cola ´queue´ para realizar el trabajo de compilación del archivo dado
 // El mensaje debe contener el nombre del archivo fuente a compilar.
-function worker(){
-    amqp.connect('amqp://localhost', function(error0, connection) {
-        if (error0)
-            throw error0;
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0)
+        throw error0;
 
-        connection.createChannel(function(error1, channel) {
-            if (error1) 
-                throw error1;
-                
-            // Esta operación es idempotente, la cola sólo se creará
-            // si no existe previamente
-            channel.assertQueue(queue, {
-                durable: true
-            });
+    connection.createChannel(function(error1, channel) {
+        if (error1) 
+            throw error1;
+            
+        // Esta operación es idempotente, la cola sólo se creará
+        // si no existe previamente
+        channel.assertQueue(queue, {
+            durable: true
+        });
 
-            // Para equilibrar la carga en el server si ejecutamos más de un worker
-            // hay que activar prefetch
-            channel.prefetch(1);
+        // Para equilibrar la carga en el server si ejecutamos más de un worker
+        // hay que activar prefetch
+        channel.prefetch(1);
 
-            channel.consume(queue, function(msg){
-                console.log("mensaje recibido.");
+        channel.consume(queue, function(msg){
+            console.log("mensaje recibido.");
 
-                let datos = JSON.parse(msg.content.toString());
-                texCompiler(datos[0],false)
-                .then(r => {
-                    channel.ack(msg);
-                })
-                .catch(e =>{
-                    throw e;
-                });  
-            },{
-                noAck: false
-            });
+            let datos = JSON.parse(msg.content.toString());
+            texCompiler(datos[0],false)
+            .then(r => {
+                channel.ack(msg);
+            })
+            .catch(e =>{
+                throw e;
+            });  
+        },{
+            noAck: false
         });
     });
-}
-
-module.exports = worker
+});
--- test/integration.test.js
+++ test/integration.test.js
@@ -3,7 +3,6 @@ const chai    = require('chai');
 const request = require('supertest');
 const expect  = chai.expect;
 const worker  = require('../src/worker');
-worker();
 
 describe('Tests de integración', function(){
     describe('GET', function(){

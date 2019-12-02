f62df2d0060711eb9fae277c49afea3225640dd2
Víctor Peralta <victorperalta.correo@gmail.com>
2019-12-02 01:47:28 +0100

    pagina de bienvenida simplificada y movida a dir principal

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(fileUpload());
-app.use(express.static('docs/views'));
+app.use(express.static('views'));
 
 //assuming app is express Object.
 app.get('/',function(req,res) {
-    res.sendFile('./docs/views/index.html');
+    res.sendFile('./views/index.html');
 });
 
 /**
